8727814ea2f95b4b1c0b6f896546eeb254cdd45e
nucasspro <hailongdang.hailong@gmail.com>
2019-01-08 14:14:59 +0700

    Add category view. But have an error when load categories

--- NUShop/NUShop.WebMVC/Areas/Admin/Controllers/CategoryController.cs
+++ NUShop/NUShop.WebMVC/Areas/Admin/Controllers/CategoryController.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NUShop.Service.Interfaces;
+using NUShop.Utilities.Helpers;
+using NUShop.ViewModel.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace NUShop.WebMVC.Areas.Admin.Controllers
 {
-    public class CategoryController : Controller
+    public class CategoryController : BaseController
     {
         #region Injections
 
@@ -24,11 +28,87 @@ namespace NUShop.WebMVC.Areas.Admin.Controllers
             return View();
         }
 
+
         [HttpGet]
-        public IActionResult GetAllCategories()
+        public IActionResult GetAll()
         {
             var model = _categoryService.GetAll();
             return new OkObjectResult(model);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var model = _categoryService.GetById(id);
+
+            return new ObjectResult(model);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateParentId(int sourceId, int targetId, Dictionary<int, int> items)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new BadRequestObjectResult(ModelState);
+            }
+
+            if (sourceId == targetId)
+            {
+                return new BadRequestResult();
+            }
+
+            _categoryService.UpdateParentId(sourceId, targetId, items);
+            return new OkResult();
+        }
+
+        [HttpPost]
+        public IActionResult SaveEntity(CategoryViewModel categoryViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                var allErrors = ModelState.Values.SelectMany(v => v.Errors);
+                return new BadRequestObjectResult(allErrors);
+            }
+
+            categoryViewModel.SeoAlias = TextHelper.ToUnsignString(categoryViewModel.Name);
+            if (categoryViewModel.Id == 0)
+            {
+                _categoryService.Add(categoryViewModel);
+            }
+            else
+            {
+                _categoryService.Update(categoryViewModel);
+            }
+            return new OkObjectResult(categoryViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult ReOrder(int sourceId, int targetId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new BadRequestObjectResult(ModelState);
+            }
+
+            if (sourceId == targetId)
+            {
+                return new BadRequestResult();
+            }
+
+            _categoryService.ReOrder(sourceId, targetId);
+            return new OkResult();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            if (id == 0)
+            {
+                return new BadRequestResult();
+            }
+
+            _categoryService.Delete(id);
+            return new OkObjectResult(id);
         }
     }
 }
--- NUShop/NUShop.WebMVC/wwwroot/app/controllers/category/index.js
+++ NUShop/NUShop.WebMVC/wwwroot/app/controllers/category/index.js
@@ -1,1 +1,80 @@
-﻿
+﻿var CategoryController = function () {
+    this.Init = function () {
+        loadCategories();
+    }
+
+   
+    function loadCategories() {
+        $.ajax({
+            url: 'GetAll',
+            dataType: 'json',
+            success: function (response) {
+                var data = [];
+                $.each(response,
+                    function (i, item) {
+                        data.push({
+                            id: item.Id,
+                            text: item.Name,
+                            parentId: item.ParentId,
+                            sortOrder: item.SortOrder
+                        });
+                    });
+
+                var treeArr = NUShopConfig.unflattern(data);
+
+                treeArr.sort(function (a, b) {
+                    return a.sortOrder - b.sortOrder;
+                });
+
+                $('#category-tree').tree({
+                    data: treeArr,
+                    dnd: true,
+                    onDrop: function (target, source, point) {
+                        console.log(target);
+                        console.log(source);
+                        console.log(point);
+                        var targetNode = $(this).tree('getNode', target);
+                        if (point === 'append') {
+                            var children = [];
+                            $.each(targetNode.children,
+                                function (i, item) {
+                                    children.push({
+                                        key: item.id,
+                                        value: i
+                                    });
+                                });
+
+                            //Update to database
+                            $.ajax({
+                                url: 'UpdateParentId',
+                                type: 'PUT',
+                                dataType: 'json',
+                                data: {
+                                    sourceId: source.id,
+                                    targetId: targetNode.id,
+                                    items: children
+                                },
+                                success: function (res) {
+                                    loadCategories();
+                                }
+                            });
+                        } else if (point === 'top' || point === 'bottom') {
+                            $.ajax({
+                                url: 'ReOrder/',
+                                type: 'POST',
+                                dataType: 'json',
+                                data: {
+                                    sourceId: source.id,
+                                    targetId: targetNode.id
+                                },
+                                success: function (res) {
+                                    loadCategories();
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
