7787163e5a02a457304e0dd87b520c17671fa2bf
nucasspro <hailongdang.hailong@gmail.com>
2019-01-16 09:53:33 +0700

    add MenuViewComponent

---
+++ NUShop/NUShop.WebMVC/Controllers/Components/MenuViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NUShop.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NUShop.WebMVC.Controllers.Components
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        #region Injections
+        private readonly ICategoryService _categoryService;
+        private readonly ILogger<MenuViewComponent> _logger;
+
+        public MenuViewComponent(ICategoryService categoryService, ILogger<MenuViewComponent>  logger)
+        {
+            _categoryService = categoryService;
+            _logger = logger;
+        }
+
+        #endregion
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = _categoryService.GetAll();
+            return View(categories);
+        }
+    }
+}
