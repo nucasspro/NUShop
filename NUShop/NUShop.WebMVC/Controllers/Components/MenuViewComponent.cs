13a85295bc66410878bd09abe47c5319d361e55e
nucasspro <hailongdang.hailong@gmail.com>
2019-01-16 10:19:43 +0700

    add MobileMenuViewComponent

--- NUShop/NUShop.WebMVC/Controllers/Components/MenuViewComponent.cs
+++ NUShop/NUShop.WebMVC/Controllers/Components/MenuViewComponent.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NUShop.Service.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace NUShop.WebMVC.Controllers.Components
@@ -11,16 +8,17 @@ namespace NUShop.WebMVC.Controllers.Components
     public class MenuViewComponent : ViewComponent
     {
         #region Injections
+
         private readonly ICategoryService _categoryService;
         private readonly ILogger<MenuViewComponent> _logger;
 
-        public MenuViewComponent(ICategoryService categoryService, ILogger<MenuViewComponent>  logger)
+        public MenuViewComponent(ICategoryService categoryService, ILogger<MenuViewComponent> logger)
         {
             _categoryService = categoryService;
             _logger = logger;
         }
 
-        #endregion
+        #endregion Injections
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
---
+++ NUShop/NUShop.WebMVC/Controllers/Components/MobileMenuViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NUShop.Service.Interfaces;
+using System.Threading.Tasks;
+
+namespace NUShop.WebMVC.Controllers.Components
+{
+    public class MobileMenuViewComponent : ViewComponent
+    {
+        #region Injections
+
+        private readonly ICategoryService _categoryService;
+        private readonly ILogger<MobileMenuViewComponent> _logger;
+
+        public MobileMenuViewComponent(ICategoryService categoryService, ILogger<MobileMenuViewComponent> logger)
+        {
+            _categoryService = categoryService;
+            _logger = logger;
+        }
+
+        #endregion Injections
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = _categoryService.GetAll();
+            return View(categories);
+        }
+    }
+}
